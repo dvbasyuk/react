02ec5d6a9931fe701b97af357fcb84d20228e888
dvbasyuk <dvbasyuk@mail.rugit config --global user.name dvbasyukgit config --global user.email dvbasyuk@mail.ru>
2020-09-21 16:22:26 +0300

    Updates

---
+++ precache-manifest.554cd161984718a21b1e97e136476e17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e45871f4667648e3bbeea8e3cb7f3f",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "429292f465bacb3d0320",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a0bda0df274e2fd30fa0",
+    "url": "/react/static/js/2.959009c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.959009c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "429292f465bacb3d0320",
+    "url": "/react/static/js/main.71fbc5f4.chunk.js"
+  },
+  {
+    "revision": "f714000af66a8e0e0556",
+    "url": "/react/static/js/runtime-main.5c6e2fee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
